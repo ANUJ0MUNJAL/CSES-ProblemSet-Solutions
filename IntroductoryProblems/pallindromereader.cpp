cdb09cf227a5e34759143f64c201928397b15518
Anuj Munjal <104863126+ANUJ0MUNJAL@users.noreply.github.com>
2024-03-09 20:38:03 +0530

    Add files via upload

---
+++ IntroductoryProblems/pallindromereader.cpp
@@ -0,0 +1,114 @@
+#include <bits/stdc++.h>
+using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;  
+#define ll long long
+#define loop(i,a,b) for(int i=a;i<b;i++)
+#define all(p) p.begin(),p.end()
+#define pb(p) push_back(p);
+int pop_count(long long a){int res=0;while(a){res+=(a&1),a>>=1;}return res;}
+template<class T> using _pq = priority_queue<T>;
+template<class T> using _pqr = priority_queue<T, vector<T>, greater<T>>;
+template<class T> void So(vector<T> &v) {sort(v.begin(),v.end());}
+template<class T> void Sore(vector<T> &v) {sort(v.begin(),v.end(),[](T x,T y){return x>y;});}
+
+template<class T> void vec_out(vector<T> &p,int ty=0){
+if(ty==2){cout<<'{';for(int i=0;i<(int)p.size();i++){if(i){cout<<",";}cout<<'"'<<p[i]<<'"';}cout<<"}\n";}
+else{if(ty==1){cout<<p.size()<<"\n";}for(int i=0;i<(int)(p.size());i++){if(i) cout<<" ";cout<<p[i];}cout<<"\n";}}
+
+template<class T> bool chmin(T &a,const T &b){if(a>b){a=b;return 1;}else return 0;}
+template<class T> bool chmax(T &a,const T &b){if(a<b){a=b;return 1;}else return 0;} 
+
+template<class T> T vec_min(vector<T> &a){assert(!a.empty());T ans=a[0];for(auto &x:a) chmin(ans,x);return ans;}
+template<class T> T vec_max(vector<T> &a){assert(!a.empty());T ans=a[0];for(auto &x:a) chmax(ans,x);return ans;}
+template<class T> T vec_sum(vector<T> &a){assert(!a.empty());T ans=a[0]-a[0];for(auto &x:a) ans+=x;return ans;}
+
+template<class T>  void matcout(vector<vector<T>> &a){for(auto &i:a){ for(auto &j:i){cout<<j<<"  ";}cout<<"\n";}cout<<"\n";}
+
+template<class T> ll LB(vector<T> &v,T a){return lower_bound(v.begin(),v.end(),a)-v.begin();}
+template<class T> ll UB(vector<T> &v,T a){return upper_bound(v.begin(),v.end(),a)-v.begin();}
+
+void solve();
+int main()
+{
+    
+        solve();
+    
+    return 0;
+}
+void solve()
+{
+    string s;
+    cin>>s;
+
+    unordered_map<char,int> mp;
+    for(auto j: s){
+        mp[j]++;
+    }
+    int count=0;
+    bool flag=true;
+    if(s.length() % 2 != 0){
+         for(auto j: mp){
+            if(j.second % 2 !=0){
+                count++;
+                if(count>1){
+                    flag=false;
+                    break;
+                }
+            }
+         }
+
+         if(flag){
+           char temp;
+           string arr;
+           int time;
+           for(auto j: mp){
+            if(j.second % 2 != 0){
+                temp=j.first;
+                time=j.second;
+                continue;
+            }
+            for(int i=0;i<j.second/2;i++){
+                arr+=j.first;
+            }
+           
+
+           }
+            string b=arr;
+            reverse(b.begin(),b.end());
+            for(int i=0;i<time;i++){
+            arr+=temp;
+            }
+            
+            arr+=b;
+            cout<<arr<<endl;
+         }else{
+            cout<<"NO SOLUTION"<<endl;
+         }
+
+    }else{
+
+        for(auto j: mp){
+            if(j.second % 2 != 0){
+                flag=false;
+            }
+        }
+         if(flag){
+            string temp;
+            for(auto j: mp){
+                for(int i=0;i<j.second/2;i++){
+                    temp+=j.first;
+                }
+            }
+            string arr=temp;
+            reverse(arr.begin(),arr.end());
+            temp=temp+arr;
+            cout<<temp<<endl;
+         }else{
+            cout<<"NO SOLUTION"<<endl;
+         }
+
+    }
+   
+}
