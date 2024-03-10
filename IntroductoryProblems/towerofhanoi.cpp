e264bb00f396f9ef2d9520044785c62fa9674709
Anuj Munjal <104863126+ANUJ0MUNJAL@users.noreply.github.com>
2024-03-10 14:38:13 +0530

    Add files via upload

---
+++ IntroductoryProblems/towerofhanoi.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;  
+#define ll long long
+#define loop(i,a,b) for(int i=a;i<b;i++)
+#define all(p) p.begin(),p.end()
+#define pb(p) push_back(p);
+int pop_count(long long a){int res=0;while(a){res+=(a&1),a>>=1;}return res;}
+template<class T> using _pq = priority_queue<T>;
+template<class T> using _pqr = priority_queue<T, vector<T>, greater<T>>;
+template<class T> void So(vector<T> &v) {sort(v.begin(),v.end());}
+template<class T> void Sore(vector<T> &v) {sort(v.begin(),v.end(),[](T x,T y){return x>y;});}
+
+template<class T> void vec_out(vector<T> &p,int ty=0){
+if(ty==2){cout<<'{';for(int i=0;i<(int)p.size();i++){if(i){cout<<",";}cout<<'"'<<p[i]<<'"';}cout<<"}\n";}
+else{if(ty==1){cout<<p.size()<<"\n";}for(int i=0;i<(int)(p.size());i++){if(i) cout<<" ";cout<<p[i];}cout<<"\n";}}
+
+template<class T> bool chmin(T &a,const T &b){if(a>b){a=b;return 1;}else return 0;}
+template<class T> bool chmax(T &a,const T &b){if(a<b){a=b;return 1;}else return 0;} 
+
+template<class T> T vec_min(vector<T> &a){assert(!a.empty());T ans=a[0];for(auto &x:a) chmin(ans,x);return ans;}
+template<class T> T vec_max(vector<T> &a){assert(!a.empty());T ans=a[0];for(auto &x:a) chmax(ans,x);return ans;}
+template<class T> T vec_sum(vector<T> &a){assert(!a.empty());T ans=a[0]-a[0];for(auto &x:a) ans+=x;return ans;}
+
+template<class T>  void matcout(vector<vector<T>> &a){for(auto &i:a){ for(auto &j:i){cout<<j<<"  ";}cout<<"\n";}cout<<"\n";}
+
+template<class T> ll LB(vector<T> &v,T a){return lower_bound(v.begin(),v.end(),a)-v.begin();}
+template<class T> ll UB(vector<T> &v,T a){return upper_bound(v.begin(),v.end(),a)-v.begin();}
+vector<vector<int>> ans;
+void towerofhanoi(int n,int a,int b,int c){
+    if(n==0)
+       return;
+    towerofhanoi(n-1,a,c,b);
+    ans.push_back({a,c});
+    towerofhanoi(n-1,b,a,c);
+}
+void solve();
+int main()
+{
+        solve();
+    
+    return 0;
+}
+void solve()
+{
+    ll n;
+    cin>>n;
+    int a=1,b=2,c=3;
+    towerofhanoi(n,a,b,c);
+    
+    cout<<ans.size()<<endl;
+    for(auto j: ans){
+        cout << j[0] << " "<< j[1]<<endl;
+    }
+    
+}
